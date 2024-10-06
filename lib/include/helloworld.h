521208681655a6081371d3ed4eefb6f6473d89dc
Vitalii Shkibtan <vetalguru@gmail.com>
2024-10-06 20:36:31 +0300

    [KAN-7] Create simple project

---
+++ lib/include/helloworld.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+
+void print_message();
+
+#endif // HELLO_WORLD_H
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "helloworld.h"
+
+int main (int argc, char* argv[])
+{
+    print_message();
+
+    return 0;
+}
+
