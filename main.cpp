42421323845abdc6dca6f2457bb6961eb8ddaaef
Vitalii Shkibtan <vetalguru@gmail.com>
2024-10-11 21:10:57 +0000

    KAN 24 Fix linter errors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include "helloworld.h"
+// Copyright 2024 Vitalii Shkibtan
 
-int main (int argc, char* argv[])
-{
+#include "lib/include/helloworld.h"
+
+int main(int argc, char* argv[]) {
     print_message();
 
     return 0;
