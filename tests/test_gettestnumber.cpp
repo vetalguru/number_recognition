902404427fa369b3057173d537c5a6932df140f6
Vitalii Shkibtan <vetalguru@gmail.com>
2024-10-11 23:52:11 +0000

    KAN-9 Add simple gtest

---
+++ tests/test_gettestnumber.cpp
@@ -0,0 +1,10 @@
+// Copyright 2024 Vitalii Shkibtan
+
+#include <gtest/gtest.h>
+
+#include "../../lib/include/helloworld.h"
+
+TEST(GetTestNumber, SimpleTest)
+{
+    EXPECT_EQ(42, GetTestNumber());
+}
