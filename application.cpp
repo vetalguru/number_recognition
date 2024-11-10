9007344a1ca322bb18d9ba4386dd846b82fffd09
Vitalii Shkibtan <vetalguru@gmail.com>
2024-11-11 00:37:10 +0200

    KAN-30 Fix linter errors

--- application.cpp
+++ application.cpp
@@ -19,31 +19,31 @@ int Application::run() const noexcept {
     return 0;
 }
 
-void Application::parseCommandLine(const int argc, const char* const argv[]) const noexcept {
+void Application::parseCommandLine(
+    const int argc, const char* const argv[]) const noexcept {
     po::options_description mainDesc{"General options:"};
     std::string taskType;
     mainDesc.add_options()
         ("help,h", "Show help")
-        ("mode,m", po::value<std::string>(&taskType), "Select mode: training, recognition")
-        ;
+        ("mode,m", po::value<std::string>(&taskType),
+            "Select mode: training, recognition");
 
     po::options_description trainDesc("Training options:");
     trainDesc.add_options()
         ("input,i", po::value<std::string>(), "Input file")
         ("info,i", po::value<std::string>(), "Input data file")
-        ("output,o", po::value<std::string>(), "Output parameters file")
-        ;
+        ("output,o", po::value<std::string>(), "Output parameters file");
 
     po::options_description recDesc("Recognition options:");
     recDesc.add_options()
         ("input,i", po::value<std::string>(), "Input file")
         ("params,p", po::value<std::string>(), "Input parameters file")
-        ("output,o", po::value<std::string>(), "Output file")
-        ;
+        ("output,o", po::value<std::string>(), "Output file");
 
     po::variables_map vm;
     po::parsed_options parsed =
-        po::command_line_parser(argc, argv).options(mainDesc).allow_unregistered().run();
+        po::command_line_parser(argc, argv)
+            .options(mainDesc).allow_unregistered().run();
 
     po::store(parsed, vm);
     po::notify(vm);
@@ -62,34 +62,42 @@ void Application::parseCommandLine(const int argc, const char* const argv[]) con
     }
 }
 
-void Application::initTrainingMode(const po::variables_map& vm) const noexcept {
+void Application::initTrainingMode(
+    const po::variables_map& vm) const noexcept {
     std::cout << "Detected training mode" << std::endl;
 
     if (vm.count("input")) {
-        std::cout << "input = " << vm["input"].as<std::string>() << std::endl;
+        std::cout << "input = "
+            << vm["input"].as<std::string>() << std::endl;
     }
 
     if (vm.count("info")) {
-        std::cout << "info = " << vm["info"].as<std::string>() << std::endl;
+        std::cout << "info = "
+            << vm["info"].as<std::string>() << std::endl;
     }
 
     if (vm.count("output")) {
-        std::cout << "output = " << vm["output"].as<std::string>() << std::endl;
+        std::cout << "output = "
+            << vm["output"].as<std::string>() << std::endl;
     }
 }
 
-void Application::initRecognitionMode(const po::variables_map& vm) const noexcept {
+void Application::initRecognitionMode(
+    const po::variables_map& vm) const noexcept {
     std::cout << "Detected recognition mode" << std::endl;
 
     if (vm.count("input")) {
-        std::cout << "input = " << vm["input"].as<std::string>() << std::endl;
+        std::cout << "input = "
+            << vm["input"].as<std::string>() << std::endl;
     }
 
     if (vm.count("params")) {
-        std::cout << "params = " << vm["params"].as<std::string>() << std::endl;
+        std::cout << "params = "
+            << vm["params"].as<std::string>() << std::endl;
     }
 
     if (vm.count("output")) {
-        std::cout << "output = " << vm["output"].as<std::string>() << std::endl;
+        std::cout << "output = "
+            << vm["output"].as<std::string>() << std::endl;
     }
 }
