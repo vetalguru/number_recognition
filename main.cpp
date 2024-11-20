adb111c9eaade796553fe8f0fcc38f31e6a35169
Vitalii Shkibtan <vetalguru@gmail.com>
2024-11-20 18:05:28 +0000

    KAN-32 Move versioning to application class

--- main.cpp
+++ main.cpp
@@ -2,16 +2,7 @@
 
 #include "./application.h"
 
-#include <iostream>
-
-#include "version.h"
-
 int main(int argc, char* argv[]) {
-    std::cout << "Version: " <<
-        static_cast<int>(VERSION_MAJOR) << "." <<
-        static_cast<int>(VERSION_MINOR) << "." <<
-        VERSION_BUILD << std::endl;
-
     Application app(argc, argv);
     return app.run();
 }
