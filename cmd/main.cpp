1da3460f201280655b5fad5e1c9a75c331b716ff
Vetal Guru <vetalguru@gmail.com>
2025-02-21 23:06:28 +0000

    KAN-38 implement header-only logger

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -2,8 +2,13 @@
 
 #include "./application.h"
 
+#include "../lib/include/logger.h"
+
 
 int main(int argc, char* argv[]) {
+    LOG_DEBUG << "Application started...";
     Application app;
-    return app.run(argc, argv);
+    int result = app.run(argc, argv);
+    LOG_DEBUG << "Application finished with code: " << result;
+    return result;
 }
